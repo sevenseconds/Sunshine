74c2477b33fe779982e98ae217175101a95d39d7
Worawat <worawat@kitchenup.com>
2016-10-21 19:47:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/fragment/MainFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.sunshine.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.sunshine.R;
+
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
